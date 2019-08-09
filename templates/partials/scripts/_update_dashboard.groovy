d4fe1d312b543f7b8cf8427cdd9567b2278faf69
Javier Hernán Caballero García <caballerojavier13@gmail.com>
2019-08-09 13:40:35 -0300

    Fix Android

--- templates/partials/scripts/_update_dashboard.groovy
+++ templates/partials/scripts/_update_dashboard.groovy
@@ -2,7 +2,7 @@ stage('Update Dashboard') {
     steps {
         script {
             try {
-                httpRequest httpMode: 'GET', responseHandle: 'NONE', url: 'https://us-central1-atenea-33daf.cloudfunctions.net/api/notifications'
+                httpRequest httpMode: 'GET', ignoreSslErrors: true, responseHandle: 'NONE', url: 'https://us-central1-atenea-33daf.cloudfunctions.net/api/notifications'
             } catch (exc) {}
         }
     }
